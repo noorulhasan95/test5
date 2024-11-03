918ca3247ee1308ea95acba1bdffdc5c4f4d4667
noorulhasan95 <147460550+noorulhasan95@users.noreply.github.com>
2024-11-04 01:22:45 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>python file test</h2>
+
+<p>HTML links are defined with the a tag. The link address is specified in the href attribute:</p>
+
+<a href="">none</a>
+
+</body>
+</html>
